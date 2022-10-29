1e2b17e1ccb5b8f4c40aef0ad04b02260794efd6
eli0009 <enlai.li@mail.mcgill.ca>
2022-10-29 13:51:39 -0400

    add support for programs search page

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 import requests
+import pprint
 from bs4 import BeautifulSoup
 import re
 
@@ -48,10 +49,10 @@ class Download:
     
 class ProgramsDownload():
 
-    def __init__(self, search):
-        self.template = f"https://www.mcgill.ca/study/2022-2023/programs/search?search_api_views_fulltext={search.strip().replace(' ', '+')}&sort_by=search_api_relevance&page="
+    def __init__(self, search, year='2022-2023'):
+        self.template = f"https://www.mcgill.ca/study/{year}/programs/search?search_api_views_fulltext={search.strip().replace(' ', '+')}&sort_by=search_api_relevance&page="
     
-        self.programs = []
+        self.programs = {}
 
     def course_search(self):
         """
@@ -59,7 +60,7 @@ class ProgramsDownload():
         """
 
         page = 0
-        while get_courses_from_program_page(self.template + page):
+        while self.get_programs_from_page(self.template + str(page)):
             page += 1
     
     def get_programs_from_page(self, url):
@@ -69,7 +70,50 @@ class ProgramsDownload():
         false otherwise
         """
 
+        has_result = False
         dl = Download(url)
+        dl.request()
+
+        programs = dl.content.find_all('a', href=True)
+        p = re.compile(r'([0-9]{2,3} credits)') # match programs
+        for program in programs:
+            m = p.search(program.text)
+            if m:
+                self.programs[program.text] = 'https://www.mcgill.ca' + program['href']
+                has_result = True
+        
+        return has_result
+
+    def __str__(self):
+        return pprint.pformat(self.programs)
+
+    def __len__(self):
+        return len(self.programs)
+
+
+class CoursesDownload():
+
+    def __init__(self, url):
+        #convert course code to url
+        self.url = url
+
+    def get_courses_from_program_page(self):
+        """
+        Get all courses code from soup. Course code should look like this:
+        MATH 222
+        COMP 551
+        ECSE 539
+        """
+        course_numbers = self.content.find_all('a',
+                                                {'class': 'program-course-title'})
+        
+        codes = []
+        p = re.compile(r'[A-Z]{4} [0-9]{3}')
+        for course_number in course_numbers:
+            m = p.search(course_number.text)
+            if m.group():
+                codes.append(m.group().replace(' ', '-'))
+        return codes
 
 class CourseDownload():
 
@@ -83,35 +127,26 @@ class CourseDownload():
             self.course_code = url
             self.url = 'https://www.mcgill.ca/study/2022-2023/courses/' + url
 
-    def get_courses_from_program_page(self):
-        """
-        Get all courses code from soup. Course code should look like this:
-        MATH 222
-        COMP 551
-        ECSE 539
-        """
-        if '/mathstat/undergraduate/programs' in self.url:
-            course_numbers = self.content.find_all('span', {'class': 'course_number'})
-            return [course_number.text.strip().replace(' ', '-')
-                    for course_number in course_numbers]
-        else:
-            course_numbers = self.content.find_all('a',
-                                                   {'class': 'program-course-title'})
-            
-            codes = []
-            p = re.compile(r'[A-Z]{4} [0-9]{3}')
-            for course_number in course_numbers:
-                m = p.search(course_number.text)
-                if m.group():
-                    codes.append(m.group().replace(' ', '-'))
-            return codes
-
-
 if __name__ == '__main__':
     
-    url_math = 'https://www.mcgill.ca/mathstat/undergraduate/programs/b-sc/minor-statistics-b-sc'
-    url = 'https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/programs/bachelor-engineering-beng-bioengineering'
+    # url_math = 'https://www.mcgill.ca/mathstat/undergraduate/programs/b-sc/minor-statistics-b-sc'
+    # url = 'https://www.mcgill.ca/study/2022-2023/faculties/engineering/undergraduate/programs/bachelor-engineering-beng-bioengineering'
 
-    dl = Download(url)
-    dl.request()
-    print(dl.get_courses_from_program_page())
+    # dl = Download(url)
+    # dl.request()
+    # print(dl.get_courses_from_program_page())
+
+    """
+    Programs search page
+    """
+    dl = ProgramsDownload('bio eng')
+    dl.course_search()
+    print(dl)
+    print(len(dl))
+
+    """
+    Courses listing page
+    """
+    url = 'https://www.mcgill.ca/study/2022-2023/faculties/engineering/graduate/programs/master-engineering-meng-materials-engineering-non-thesis'
+
+
